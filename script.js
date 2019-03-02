c5986cfa6da15be6e7cbbccf00f77f9e6a34f388
Chandini Ragobar <chandiniceline@gmail.com>
2019-03-02 19:46:36 +0000

    advice slip

---
+++ script.js
@@ -0,0 +1,12 @@
+let request = new XMLHttpRequest();
+let url = "https://api.adviceslip.com/advice/search/hug";
+request.open("GET", url, true);
+request.onload = function(){
+    let data = JSON.parse(this.response);
+    let advice = document.getElementById('advice');
+    if (request.status >= 200 && request.status < 400 ){
+        console.log('data'.data)
+        advice.innerHTML = data.slips[0].advice;
+    }
+};
+request.send();
